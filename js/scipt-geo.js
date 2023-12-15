e2daaf2a13922555b8b1b72679a9c3abfae72edf
nvostokov <103598346+nvostokov@users.noreply.github.com>
2023-12-15 13:52:07 +0300

    Add files via upload

---
+++ js/scipt-geo.js
@@ -0,0 +1,36 @@
+/* const mq = window.matchMedia('(max-width: 2100px)')
+ */
+let scaleMap
+
+/* if (mq.matches) {
+    // ширина окна меньше, чем 2100px
+    scale = 12
+} else {
+    // ширина окна больше, чем 2100px
+    scale = 14
+} */
+
+let windowWidth = window.innerWidth
+
+/* alert(windowWidth)
+
+let windowHeight = window.innerHeight
+
+alert(windowHeight) */
+
+
+
+if (windowWidth > 2600) {
+  scaleMap = 14
+}
+else if (windowWidth < 1400 ) {
+  scaleMap = 12
+}
+else
+{
+  scaleMap = 13
+}
+
+/* alert(windowWidth)
+ */
+console.log(scaleMap, windowWidth, window.innerHeight)
